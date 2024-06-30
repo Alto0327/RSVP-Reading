6d17d37f32496e02836f25bb7c9aec89cfb496a0
Alto0327 <aj.silvaf2003@gmail.com>
2024-06-30 13:42:49 -0400

    Created functioning slider and area to display wpm for users to see

--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,28 @@ const DelayedAction = () => {
     });
   };
 
-  
+ let WPM = Math.round(60000/delayDuration)
+
   return (
     <div>
       <h1>What is RSVP reading?</h1>
       <p>Rapid serial visual presentation (RSVP) is a method for bypassing eye movements during reading. In RSVP, each word (or small group of words) appears in the same location, serially.</p>
       <h1>{actionStatus}</h1>
       <button onClick={handleButtonClick}>Start Reading</button>
-      <input
-        type="number"
-        value={delayDuration}
-        onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
-      />
       <input 
-        type="range" min="1" max="10"  
+        type="range" min="10" max="1000"
+        value={delayDuration} 
+        onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))} 
       /> 
     {/* Make Range bar the wpm selector move the text input*/} 
+      <input type="text"
+        value={WPM}
+        onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))} 
+        //Above code causes error here so need to create a new function to hanlde WPM change 
+        //1.Calculate delay duration from WPM when the user inputs WPM.
+        //Update the delay duration accordingly.
+      />
+      <p>Words Per minute: {WPM}</p>
       <textarea 
         value={text} 
         onChange={event => setText(event.target.value)} 
