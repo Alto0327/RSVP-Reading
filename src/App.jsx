41dd909396047daa2309187d5f2f65fb780e52ea
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-02 16:08:08 -0400

    Prettier Code

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,12 @@ const DelayedAction = () => {
     });
   };
 
-
   const handleWpmChange = (e) => {
-    const newWPM = parseInt(e.target.value, 10)
-    if (!isNaN(newWPM) && newWPM > 0){
+    const newWPM = parseInt(e.target.value, 10);
+    if (!isNaN(newWPM) && newWPM > 0) {
       setDelayDuration(Math.round(60000 / newWPM));
     }
-  }
+  };
 
   const Wpm = Math.round(60000 / delayDuration);
 
@@ -31,48 +30,47 @@ const DelayedAction = () => {
     <div>
       <div className="container">
         <section className="leftSide-Main">
-      <h1>What is RSVP reading?</h1>
-      <p>
-        Rapid serial visual presentation (RSVP) is a method for bypassing eye
-        movements during reading. In RSVP, each word (or small group of words)
-        appears in the same location, serially.
-      </p>
-      <iframe
+          <h1>What is RSVP reading?</h1>
+          <p>
+            Rapid serial visual presentation (RSVP) is a method for bypassing
+            eye movements during reading. In RSVP, each word (or small group of
+            words) appears in the same location, serially.
+          </p>
+          <iframe
             src="https://www.youtube.com/embed/5yddeRrd0hA&ab"
             frameborder="0"
           ></iframe>
-          </section>
-          <section className="rightSide-Main">
-      <h1>{actionStatus}</h1>
-      <button onClick={handleButtonClick}>Start Reading</button>
-      <input //set indentation on Slider to sorta lock onto set WPM speeds
-        type="range"
-        min="100"
-        max="1000"
-        value={delayDuration}
-        onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
-      />
-      <select value={Wpm} onChange={handleWpmChange}>
-        <option value={80}>80 WPM</option>
-        <option value={120}>120 WPM</option>
-        <option value={160}>160 WPM</option>
-        <option value={200}>200 WPM</option>
-        <option value={240}>240 WPM</option>
-        <option value={280}>280 WPM</option>
-        <option value={320}>320 WPM</option>
-      </select>
-      <p>Words Per minute: {Wpm}</p>
-      <textarea
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-        rows={4}
-        cols={40}
-      />
-              </section>
-              </div>
+        </section>
+        <section className="rightSide-Main">
+          <h1>{actionStatus}</h1>
+          <button onClick={handleButtonClick}>Start Reading</button>
+          <input //set indentation on Slider to sorta lock onto set WPM speeds
+            type="range"
+            min="100"
+            max="1000"
+            value={delayDuration}
+            onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
+          />
+          <select value={Wpm} onChange={handleWpmChange}>
+            <option value={80}>80 WPM</option>
+            <option value={120}>120 WPM</option>
+            <option value={160}>160 WPM</option>
+            <option value={200}>200 WPM</option>
+            <option value={240}>240 WPM</option>
+            <option value={280}>280 WPM</option>
+            <option value={320}>320 WPM</option>
+          </select>
+          <p>Words Per minute: {Wpm}</p>
+          <textarea
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            rows={4}
+            cols={40}
+          />
+        </section>
+      </div>
     </div>
   );
 };
-
 
 export default DelayedAction;
