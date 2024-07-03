a7d3089a8cf0a94a0d7671db20114b17009d80da
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-02 22:54:59 -0400

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const DelayedAction = () => {
   const [delayDuration, setDelayDuration] = useState(500);
   const [text, setText] = useState("");
 
+
   const handleButtonClick = () => {
     const words = text.split(" ");
 
@@ -29,7 +30,7 @@ const DelayedAction = () => {
     <div>
       <div className="container">
         <section className="leftSide-Main">
-          <h1>RSVP reading</h1>
+          <h1>RSVP <br/>Reading</h1>
           <h2>Rapid Serial Visual Presentation</h2>
           <p>
             Rapid serial visual presentation (RSVP) is a method for bypassing
@@ -43,17 +44,9 @@ const DelayedAction = () => {
           ></iframe>
         </section>
         <section className="rightSide-Main">
-          <h1>{actionStatus}</h1>
-          {/* Button Should be After the Text Paragraph */}
-          <button onClick={handleButtonClick}>Start Reading</button> 
-          <input
-            type="range"
-            min="100"
-            max="1000"
-            value={delayDuration}
-            onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
-          />
+
           <select value={Wpm} onChange={handleWpmChange}>
+            <option value="" default   >WPM options</option>
             <option value={80}>80 WPM</option>
             <option value={120}>120 WPM</option>
             <option value={160}>160 WPM</option>
@@ -62,18 +55,32 @@ const DelayedAction = () => {
             <option value={280}>280 WPM</option>
             <option value={326}>326 WPM</option>
           </select>
+          <input className="Wpm-slider"
+            type="range"
+            min="100"
+            max="1000"
+            value={delayDuration}
+            onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
+            />
           <p>Words Per minute: {Wpm}</p>
+          
           {/* To Add n fix
             1. Make Text Area Larger
             2. Add word n pdf File only uploader
             3. Add a Remaining word char on bottom right of Text Area
-          */}
-          <textarea
-            value={text}
-            onChange={(event) => setText(event.target.value)}
-            rows={4}
-            cols={40}
-          />
+            */}
+          <div className="textUpload">
+            <textarea
+              value={text}
+              onChange={(event) => setText(event.target.value)}
+              rows={10}
+              cols={60}
+              placeholder="paste or upload file to RSVP read"
+            />
+            <input type="file" accept=".doc, .docx, .pdf" />
+          </div>
+          <button onClick={handleButtonClick}>Start Reading</button>
+          <h1>{actionStatus}</h1> {/* Button opens modal that displays Text screen, implement a play and pause button, rest button too*/}
         </section>
       </div>
     </div>
