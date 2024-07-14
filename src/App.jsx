d14ac1ce1a966ffa0bdffd83bfba3e7004144fff
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-14 09:41:37 -0400

    Created Set WPM section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import React, { useState } from "react";
 
-
 const DelayedAction = () => {
   const [actionStatus, setActionStatus] = useState("Text will Appear here");
   const [delayDuration, setDelayDuration] = useState(500);
   const [text, setText] = useState("");
-  const [sampleStatus, setSampleStatus] = useState("Sample RSVP at 140 wpm")
-
+  const [sampleStatus, setSampleStatus] = useState("Sample RSVP at 140 wpm");
 
   const handleButtonClick = () => {
     const words = text.split(" ");
@@ -25,13 +23,14 @@ const DelayedAction = () => {
       setDelayDuration(Math.round(60000 / newWPM));
     }
   };
-   const sample = "RSVP (Rapid Serial Visual Presentation) reading is a speed-reading technique where text is shown one word or small chunk at a time in a fixed position on a screen. This method minimizes eye movement allowing readers to focus solely on processing words, thus potentially increasing reading speed. By keeping the text centrally located and controlling the pace, RSVP can help improve focus and concentration.";
+  const sample =
+    "RSVP (Rapid Serial Visual Presentation) reading is a speed-reading technique where text is shown one word or small chunk at a time in a fixed position on a screen. This method minimizes eye movement allowing readers to focus solely on processing words, thus potentially increasing reading speed. By keeping the text centrally located and controlling the pace, RSVP can help improve focus and concentration.";
 
-  const handleSampleClick= () => {
+  const handleSampleClick = () => {
     const sampleDisplay = sample.split(" ");
     sampleDisplay.push("Click to Replay");
     console.log(sampleDisplay);
-    
+
     sampleDisplay.forEach((sampleDisplay, index) => {
       setTimeout(() => {
         setSampleStatus(sampleDisplay);
@@ -39,8 +38,6 @@ const DelayedAction = () => {
     });
   };
   const Wpm = Math.round(60000 / delayDuration);
-
-
 
   return (
     <div className="Container">
@@ -51,14 +48,11 @@ const DelayedAction = () => {
         </h1>
         <div className="Description">
           <h2>Rapid Serial Visual Presentation</h2>
-          <p>
-            {sample}
-          </p>
+          <p>{sample}</p>
         </div>
-        <div className="Sample-rsvp" >
+        <div className="Sample-rsvp">
           <h2 onClick={handleSampleClick}>{sampleStatus}</h2>
         </div>
-        
       </div>
       <section className="Container__right">
         <h2>Insert Text Here:</h2>
@@ -74,28 +68,40 @@ const DelayedAction = () => {
           {/*This uploader does nothing currently 
             Change button to be more sleek */}
         </div>
-        <select value={Wpm} onChange={handleWpmChange} className="Wpm-select">
-          <option value="" default>
-            WPM options
-          </option>
-          <option value={80}>80 WPM</option>
-          <option value={120}>120 WPM</option>
-          <option value={160}>160 WPM</option>
-          <option value={200}>200 WPM</option>
-          <option value={240}>240 WPM</option>
-          <option value={280}>280 WPM</option>
-          <option value={326}>326 WPM</option>
-        </select>
-        <input
-          className="Wpm-slider"
-          type="range"
-          min="100"
-          max="1000"
-          value={delayDuration}
-          onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
-        />
-        <p className="Wpm-display">Words Per minute: {Wpm}</p>
-        <button onClick={handleButtonClick}>Start Reading</button>
+        <h3 className="">Set WPM</h3>
+        <section className="Wpm__selectors">
+          <div>
+          <label>Select from Preset:</label>
+          <select value={Wpm} onChange={handleWpmChange} className="Wpm-select">
+            <option value="" default>
+              WPM options
+            </option>
+            <option value={80}>80 WPM</option>
+            <option value={120}>120 WPM</option>
+            <option value={160}>160 WPM</option>
+            <option value={200}>200 WPM</option>
+            <option value={240}>240 WPM</option>
+            <option value={280}>280 WPM</option>
+            <option value={326}>326 WPM</option>
+          </select>
+          </div>
+
+          <div>
+          <p>Manually set:</p>
+          <input
+            className="Wpm-slider"
+            type="range"
+            min="100"
+            max="1000"
+            value={delayDuration}
+            onChange={(e) => setDelayDuration(parseInt(e.target.value, 10))}
+            />
+          <p className="Wpm-display">Words Per minute: {Wpm}</p>
+          </div>
+        </section>
+        <button className="RSVP--button" onClick={handleButtonClick}>
+          Start Reading
+        </button>
         <h1>{actionStatus}</h1>
         {/* Button opens modal that displays Text screen, implement a play and pause button, rest button too*/}
       </section>
