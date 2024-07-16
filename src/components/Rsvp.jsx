73444b3c53db8091491af81e27e7b9d78f73f6f7
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-16 19:25:38 -0400

    Added component template and link to return to App

---
+++ src/components/Rsvp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const RSVP = () => {
+  return (
+    <div>
+      <Link to="/App">
+        <p>Return</p>
+      </Link>
+      RSVP Component
+    </div>
+  );
+};
+
+export default RSVP;
