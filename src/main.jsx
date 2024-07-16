cd4dd9aa761a7f143860b628a508e0c30c5014b4
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-16 19:28:45 -0400

    Set up Links

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import RSVP from "./components/Rsvp.jsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />}/>
+        <Route path='/App' element={<App/>}/>
+        <Route path='/RSVP' element={<RSVP/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
